6179b54623a17bfe5d3e25f97f4057fae6320266
Gabriel Carvalho <biel.brandao2004@gmail.com>
2021-05-02 15:14:23 -0300

    Adicionada a tela para caso o usuario pule a etapa de colocar o nome

--- routes.js
+++ routes.js
@@ -1,10 +1,15 @@
 const router = require('express/lib/router')
 const express = require('express')
+const fs = require('fs')
 
 const routes = new router()
 
 routes.post('/chat', (req, res) => {
     res.render('../public/chat', { nome: req.body.nome })
+})
+
+routes.get('/chat', (req, res) => {
+    res.end(fs.readFileSync(`${__dirname}/public/chat.html`))
 })
 
 routes.use(express.static('public'))
