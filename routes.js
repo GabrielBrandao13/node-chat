0b97daa9bacd83682399f9d2f5873730ac43eea3
Gabriel C. Brandão <62272513+GabrielBrandao13@users.noreply.github.com>
2021-05-19 16:38:54 -0300

    Alterada estrutura das páginas

--- routes.js
+++ routes.js
@@ -5,12 +5,15 @@ const fs = require('fs')
 const routes = new router()
 
 routes.post('/chat', (req, res) => {
-    res.render('../public/chat', { nome: req.body.nome })
+    const name = req.body.nome
+    if (name) {
+        res.render('../public/chat', { nome: req.body.nome })
+    } else {
+        res.end(fs.readFileSync(`${__dirname}/public/chat.html`))
+    }
+
 })
 
-routes.get('/chat', (req, res) => {
-    res.end(fs.readFileSync(`${__dirname}/public/chat.html`))
-})
 
 routes.use(express.static('public'))
 
